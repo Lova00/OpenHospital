Lova00/OpenHospital

commit 2cd2e1e4eb79758ed94670644b9c5a4acaba71ce

OP-2 #comment Fixed current user retrieving #time 30m

src/org/isf/menu/manager/UserBrowsingManager.java
@@ -15,6 +15,11 @@ public class UserBrowsingManager {
 
 	private MenuIoOperations ioOperations = Context.getApplicationContext().getBean(MenuIoOperations.class);
 	
+	
+	public static String getCurrentUser() {
+		return UserBrowsingManager.getCurrentUser();
+	}
+	
 	/**
 	 * returns the list of {@link User}s
 	 * @return the list of {@link User}s

src/org/isf/accounting/gui/BillBrowser.java
@@ -53,6 +53,7 @@ import org.isf.generaldata.MessageBundle;
 import org.isf.hospital.manager.HospitalBrowsingManager;
 import org.isf.menu.gui.MainMenu;
 import org.isf.menu.manager.Context;
+import org.isf.menu.manager.UserBrowsingManager;
 import org.isf.stat.gui.report.GenericReportBill;
 import org.isf.stat.gui.report.GenericReportFromDateToDate;
 import org.isf.stat.gui.report.GenericReportUserInDate;
@@ -156,7 +157,7 @@ public class BillBrowser extends ModalJFrame implements PatientBillListener {
 	private String currencyCod = null;
 	
 	//Users
-	private String user = MainMenu.getUser();
+	private String user = UserBrowsingManager.getCurrentUser();
 	private ArrayList<String> users;
 	
 	public BillBrowser() {
@@ -330,7 +331,7 @@ public class BillBrowser extends ModalJFrame implements PatientBillListener {
 							if (GeneralData.SINGLEUSER) {
 								user = "admin";
 							} else {
-								user = MainMenu.getUser();
+								user = UserBrowsingManager.getCurrentUser();
 							}
 							try {
 								BusyState.setBusyState(BillBrowser.this, true);
@@ -982,7 +983,7 @@ public class BillBrowser extends ModalJFrame implements PatientBillListener {
 	private void updateTotals() {
 		ArrayList<Bill> billToday = null;
 		ArrayList<BillPayments> paymentsToday = null;
-		if (MainMenu.getUser().equals("admin")) {
+		if (UserBrowsingManager.getCurrentUser().equals("admin")) {
 			try {
 				billToday = billManager.getBills(dateToday0, dateToday24);
 				paymentsToday = billManager.getPayments(dateToday0, dateToday24);

src/org/isf/accounting/gui/PatientBillEdit.java
@@ -49,8 +49,8 @@ import org.isf.generaldata.GeneralData;
 import org.isf.generaldata.MessageBundle;
 import org.isf.generaldata.TxtPrinter;
 import org.isf.hospital.manager.HospitalBrowsingManager;
-import org.isf.menu.gui.MainMenu;
 import org.isf.menu.manager.Context;
+import org.isf.menu.manager.UserBrowsingManager;
 import org.isf.patient.gui.SelectPatient;
 import org.isf.patient.gui.SelectPatient.SelectionListener;
 import org.isf.patient.manager.PatientBrowserManager;
@@ -236,7 +236,7 @@ public class PatientBillEdit extends JDialog implements SelectionListener {
 	private int payItemsSaved;
 	
 	//User
-	private String user = MainMenu.getUser();
+	private String user = UserBrowsingManager.getCurrentUser();
 	
 	public PatientBillEdit() {
 		initCurrencyCod();

src/org/isf/admission/gui/AdmissionBrowser.java
@@ -69,6 +69,7 @@ import org.isf.examination.model.PatientExamination;
 import org.isf.generaldata.GeneralData;
 import org.isf.generaldata.MessageBundle;
 import org.isf.menu.gui.MainMenu;
+import org.isf.menu.manager.UserBrowsingManager;
 import org.isf.operation.manager.OperationBrowserManager;
 import org.isf.operation.model.Operation;
 import org.isf.patient.gui.PatientSummary;
@@ -1987,7 +1988,7 @@ public class AdmissionBrowser extends JDialog {
 						}// isPregnancy
 
 						// set not editable fields
-						String user = MainMenu.getUser();
+						String user = UserBrowsingManager.getCurrentUser();
 	//					String admUser = admission.getUserID();
 	//					if (admUser != null && !admUser.equals(user)) {
 	//						int yes = JOptionPane.showConfirmDialog(AdmissionBrowser.this, MessageBundle.getMessage("angal.admission.youaresigningnewdatawithyournameconfirm"));

src/org/isf/dicom/gui/DicomLoader.java
@@ -2,7 +2,6 @@ package org.isf.dicom.gui;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Window;
 
 import javax.swing.JFrame;
 import javax.swing.JLabel;

src/org/isf/menu/gui/MainMenu.java
@@ -71,10 +71,6 @@ public class MainMenu extends JFrame implements ActionListener, Login.LoginListe
 		return false;
 	}
 
-	public static String getUser() {
-		return myUser.getUserName();
-	}
-
 	private int minButtonSize = 0;
 
 	public void setMinButtonSize(int value) {

src/org/isf/menu/gui/PrivilegeTree.java
@@ -165,7 +165,7 @@ class PrivilegeTree extends JDialog {
 		UserMenuItem umi = (UserMenuItem) node.getUserObject();
 		
 		/* Also if node has leafs can be deactivated */
-		String user = MainMenu.getUser();
+		String user = UserBrowsingManager.getCurrentUser();
 		String umiFile = umi.getCode();
 		
 		if (user.equals("admin") && (umiFile.equals("file") ||

src/org/isf/opd/gui/OpdEdit.java
@@ -53,7 +53,7 @@ import org.isf.disease.model.Disease;
 import org.isf.distype.manager.DiseaseTypeBrowserManager;
 import org.isf.distype.model.DiseaseType;
 import org.isf.generaldata.MessageBundle;
-import org.isf.menu.gui.MainMenu;
+import org.isf.menu.manager.UserBrowsingManager;
 import org.isf.opd.manager.OpdBrowserManager;
 import org.isf.opd.model.Opd;
 import org.isf.utils.exception.OHServiceException;
@@ -510,7 +510,7 @@ public class OpdEdit extends JDialog {
 					opd.setDisease3(disease3);
 					opd.setVisitDate(gregDate);
 					opd.setNote("");
-					opd.setUserID(MainMenu.getUser());
+					opd.setUserID(UserBrowsingManager.getCurrentUser());
 					
 					try {
 						if (insert){    //Insert

commit 5bd2f70f85889f86a05696a733a3aac92d77a3a8

OP-1 #comment Fixed a bug introduced with 2cd2e1e

src/org/isf/menu/manager/UserBrowsingManager.java
@@ -10,6 +10,7 @@ import org.isf.menu.service.MenuIoOperations;
 import org.isf.utils.exception.OHServiceException;
 import org.isf.utils.exception.model.OHExceptionMessage;
 import org.isf.utils.exception.model.OHSeverityLevel;
+import org.slf4j.MDC;
 
 public class UserBrowsingManager {
 
@@ -17,7 +18,7 @@ public class UserBrowsingManager {
 	
 	
 	public static String getCurrentUser() {
-		return UserBrowsingManager.getCurrentUser();
+		return MDC.get("OHUser");
 	}
 	
 	/**